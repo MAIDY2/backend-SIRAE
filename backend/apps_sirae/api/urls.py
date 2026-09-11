63946e3d6ec8136a3e19d27af6e49f1a9535fcb2
Santiago Acosta <santiagobarrera2004s@gmail.com>
2026-09-11 17:19:42 -0500

    subo actualizacion

--- backend/apps_sirae/api/urls.py
+++ backend/apps_sirae/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
 
-
 urlpatterns = [
+    path('pasos-preparacion/', include('apps_sirae.pasospreparacion.api.urls')),
+    path('preparacion-asignada/', include('apps_sirae.preparacion_asignada.api.urls')),
+    path('contratos/', include('apps_sirae.contratos.api.urls')),
     path('asistencia-diaria/', include('apps_sirae.asistencia_diaria.urls')),
     path('entregas/', include('apps_sirae.entregas.urls')),
+    path('movimientos-inventario/', include('apps_sirae.movimientos_inventario.urls')),
     path('notificaciones/', include('apps_sirae.notificaciones.urls')),
 ]
