69e3d850d9b8f9185496725b96f5ee453dfe6bf3
jhonfredyr <riveracjhonf@gmail.com>
2026-09-12 12:13:50 -0500

    solucion de problemas de login

--- backend/apps_sirae/inventario/models.py
+++ backend/apps_sirae/inventario/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 class Inventario(models.Model):
+
     id_inventario = models.AutoField(primary_key=True)
     id_ingrediente = models.IntegerField()
     cantidad_actual = models.DecimalField(max_digits=10, decimal_places=2)
     stock_minimo = models.DecimalField(max_digits=10, decimal_places=2)
     id_unidad_medida = models.IntegerField()
-
     class Meta:
         db_table = 'inventario'
-        managed = False
 
     def __str__(self):
         return f"Inventario {self.id_inventario} - Ingrediente {self.id_ingrediente}"
