a300620506aed8d0312b5b1436cbebca72b29a6f
isabelacodderM <campoisabela07@gmail.com>
2026-09-16 10:31:28 -0500

    Configura API de unidades de medida

--- backend/apps_sirae/unidades_medida/api/serializer.py
+++ backend/apps_sirae/unidades_medida/api/serializer.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from ..models import UnidadMedida
 
-
 class UnidadMedidaSerializer(serializers.ModelSerializer):
     class Meta:
         model = UnidadMedida
--- backend/apps_sirae/unidades_medida/api/views.py
+++ backend/apps_sirae/unidades_medida/api/views.py
@@ -2,7 +2,6 @@ from rest_framework.viewsets import ModelViewSet
 from apps_sirae.unidades_medida.models import UnidadMedida
 from apps_sirae.unidades_medida.api.serializer import UnidadMedidaSerializer
 
-
 class UnidadMedidaApiViewSet(ModelViewSet):
     serializer_class = UnidadMedidaSerializer
     queryset = UnidadMedida.objects.all()
