eba791cb51009fe3de52f9acdb2a042c218be35a
jhonfredyr <riveracjhonf@gmail.com>
2026-09-18 19:27:45 -0500

    cambios

--- backend/apps_sirae/unidades_medida/api/views.py
+++ backend/apps_sirae/unidades_medida/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny # <--- 1. Importa esto
 from apps_sirae.unidades_medida.models import UnidadMedida
 from apps_sirae.unidades_medida.api.serializer import UnidadMedidaSerializer
 
@@ -6,3 +7,4 @@ from apps_sirae.unidades_medida.api.serializer import UnidadMedidaSerializer
 class UnidadMedidaApiViewSet(ModelViewSet):
     serializer_class = UnidadMedidaSerializer
     queryset = UnidadMedida.objects.all()
+    permission_classes = [AllowAny] # <--- 2. Agrégalo aquí para quitar el requisito de login temporalmente
