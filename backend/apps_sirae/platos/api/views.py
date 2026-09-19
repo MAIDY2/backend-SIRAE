f8bdc00e43bb9b50946de2d929a470c07924b29e
isabelacodderM <campoisabela07@gmail.com>
2026-09-18 19:33:11 -0500

    Revertir AllowAny para restaurar seguridad en Platos

--- backend/apps_sirae/platos/api/views.py
+++ backend/apps_sirae/platos/api/views.py
@@ -1,11 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny
 
 from ..models import Plato
 from .serializer import PlatoSerializer
 
 
 class PlatoApiViewSet(ModelViewSet):
-    permission_classes = [AllowAny]
     serializer_class = PlatoSerializer
     queryset = Plato.objects.all().select_related('id_seccion').order_by('nombre_plato')
