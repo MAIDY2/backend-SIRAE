a849f2cc54b9d540b50bd7f32643ae37e96f09de
jhonfredyr <riveracjhonf@gmail.com>
2026-09-24 19:33:46 -0500

    cambios propios

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,15 +18,12 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Endpoints de la API
     path('api/', include('apps_sirae.api.urls')),
 
-    # Rutas individuales (opcional si ya están dentro de apps_sirae.api.urls)
     path('api/jornadas/', include('apps_sirae.jornadas.api.urls')),
     path('api/categorias-inventario/', include('apps_sirae.categorias_inventario.api.urls')),
     path('api/menus/', include('apps_sirae.menus.api.urls')),
 
-    # Documentación Swagger y Redoc
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
