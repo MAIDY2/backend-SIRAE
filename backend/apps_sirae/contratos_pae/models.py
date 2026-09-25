3c49de66e4ecd704a5d08958a2259519a3c65a83
unknown <poolgomez27@gmail.com>
2026-09-25 15:39:02 -0500

    subo cambios en las apps de jornadas, contratos,preparacion,usuario_turno

--- backend/apps_sirae/contratos_pae/models.py
+++ backend/apps_sirae/contratos_pae/models.py
@@ -3,15 +3,16 @@ from django.db import models
 
 class Contrato(models.Model):
     id_contrato = models.AutoField(primary_key=True)
-    numero_cor = models.CharField(max_length=100)
-    institucion = models.CharField(max_length=100)
-    zona = models.CharField(max_length=100)
-    fecha_inicio = models.DateField()
-    fecha_fin = models.DateField()
-    estado = models.CharField(max_length=50, default='Activo')
+    numero_cor = models.CharField(max_length=100, null=True, blank=True)
+    institucion = models.CharField(max_length=150, null=True, blank=True)
+    zona = models.CharField(max_length=100, null=True, blank=True)
+    fecha_inicio = models.DateField(null=True, blank=True)
+    fecha_fin = models.DateField(null=True, blank=True)
+    estado = models.CharField(max_length=50, null=True, blank=True)
 
     class Meta:
-        db_table = 'contrato_pae'  # Le indica a Django el nombre real de la tabla en Render
+        db_table = 'contrato_pae'
+        managed = False
 
     def __str__(self):
         return f"Contrato {self.numero_cor} - {self.institucion} ({self.estado})"
